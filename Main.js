edfe82a392fa9d3571895ee282cece7564e22fcb
Johnnybells <johnbellring123@gmail.com>
2021-03-10 02:40:52 -0700

    website

--- Main.js
+++ Main.js
@@ -1,7 +1,19 @@
 var email = document.getElementById("e")
 var pass = document.getElementById("p")
 var Login = document.getElementById("btn")
+var loggedin = false
 
-Login.onclick(function(){
-    document.write("click")
+Login.addEventListener("onclick", function(){
+    if(loggedin == false){
+        if(email.innerHTML == "Asshole" && pass.innerHTML == "Bitch"){
+            document.write("Logged in")
+            email.innerHTML = ""
+            pass.innerHTML = ""
+    
+        }else{
+            window.alert("Failed! Try again.")
+        }
+    }else{
+        window.alert("Already Logged in")
+    }
 })
