2229b92a11a383c21599f1168a9485bca0f52781
Johnnybells <johnbellring123@gmail.com>
2021-03-10 02:29:21 -0700

    website

---
+++ Main.js
@@ -0,0 +1,7 @@
+var email = document.getElementById("e")
+var pass = document.getElementById("p")
+var Login = document.getElementById("btn")
+
+Login.onclick(function(){
+    document.write("click")
+})
